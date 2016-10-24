c48c768b663b588047be2e8cab72e8ab9762c824
Nicolas Nascimento <nicolasnascimento@MacBook-Pro-de-Nicolas.local>
2016-10-24 13:09:12 -0200

    beginning implementation

--- TF/Application.c
+++ TF/Application.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <unistd.h>
 #include <stdlib.h>
 #include <net/if.h>
 #include <net/if_arp.h>
@@ -10,14 +11,43 @@
 #include <netinet/in.h>
 #include <sys/socket.h>
 
-void cleanUp(void) {
+/// A flag to indicate wheter debug printing should be performed
+int verboseMode = 0;
+
+void getInitialFlags(int argc, char** argv) {
+	int value = 0;
+	// Loop to look for flags
+	while( (value = getopt(argc, argv, "v:")) != -1 ) {
+		switch( value ) {
+			case 'v':
+				verboseMode = 1;
+				break;
+			case '?':
+				break;
+			default:
+				break;
+		}	
+	}
+}
+
+/// Alloc & Initialze objects. 
+/// Perform Initial Setup of the program
+void init(void) {
+	
+}
+
+/// Dealloc objects
+/// Perform deinitialization for the objects
+void deinit(void) {
 
 }
 
-int main(int argc, const char* argv[]) {		
-	
+int main(int argc, char** argv) {		
+	// Gets Initial Flags
+	getInitialFlags(argc, argv);
+
 	// Register exit function
-	atexit(cleanUp);	
+	atexit(deinit);	
 	
 	// End of program
 	exit(EXIT_SUCCESS);
